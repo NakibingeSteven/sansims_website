54d0bb8915a3dbb2e19f7a1502927c664d53f7b9
Nakibinge Steven Adrian <88508645+NakibingeSteven@users.noreply.github.com>
2025-10-01 03:03:22 +0300

    adding taglines

--- app.py
+++ app.py
@@ -17,5 +17,6 @@ def technology():
     return render_template('technology.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use env PORT or default 5000
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
